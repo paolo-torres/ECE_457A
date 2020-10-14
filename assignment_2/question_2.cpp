6767b1511de0d4e962a150005d5982274a24efc0
Paolo Torres <paolo-torres@hotmail.com>
2020-10-14 04:15:51 -0400

    start question 2 bfs

--- assignment_2/question_2.cpp
+++ assignment_2/question_2.cpp
@@ -1,11 +1,13 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
+#include <queue>
 #include <utility>
 #include <vector>
 
 using namespace std;
 
-const vector<vector<const char>> maze {
+const vector<vector<const char> > maze {
     {'1', '1', '1', '1', '0', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '0', '1', '1', '1', '1', '1', '1'},
     {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
     {'0', '0', '0', '0', '0', '1', '1', '1', '0', '1', '1', '1', '1', '1', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '1'},
@@ -33,52 +35,120 @@ const vector<vector<const char>> maze {
     {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}
 };
 
-bool dfs(vector<vector<bool>> visited, vector<pair<int, int>> &path, int &nodes, int i, int j, int endX, int endY) {
-    ++nodes;
-    if (!visited[i][j]) {
-        visited[i][j] = true;
-        path.push_back(make_pair(i, j));
-        if (i == endX && j == endY) {
+bool bfs(vector<vector<int> > &path, map<vector<int>, vector<vector<int>> > &m, int &cost, int &nodes, int i, int j, int iEnd, int jEnd) {
+    vector<vector<bool> > visited(maze.size(), vector<bool>(maze[0].size(), false));
+    vector<vector<int> > dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    queue<vector<int> > q;
+    visited[i][j] = true;
+    q.push({i, j});
+    while (!q.empty()) {
+        vector<int> s = q.front();
+        q.pop();
+        if (s[0] == iEnd && s[1] == jEnd) {
             return true;
         }
-        if (j > 0 && maze[i][j-1] != '0') {
-            if (dfs(visited, path, nodes, i, j - 1, endX, endY)) {
-                return true;
+        for (int i = 0; i < dirs.size(); ++i) {
+            int x = s[0] + dirs[i][0];
+            int y = s[1] + dirs[i][1];
+            if (x >= 0 && x < maze.size() && y >= 0 && y < maze[0].size()) {
+                if (!visited[x][y] && maze[x][y] != 0) {
+                    visited[x][y] = true;
+                    q.push({x, y});
+                    if (m.find({s[0], s[1]}) == m.end()) {
+                        m.insert({{s[0], s[1]}, vector<vector<int>>()});
+                    }
+                    m[{s[0], s[1]}].push_back(q.back());
+                    if (maze[x][y] == '*') {
+                        cost += 30;
+                    } else {
+                        ++cost;
+                    }
+                    ++nodes;
+                }
             }
         }
-        if (i > 0 && maze[i-1][j] != '0') {
-            if (dfs(visited, path, nodes, i - 1, j, endX, endY)) {
-                return true;
-            }
-        }
-        if (j < maze[0].size() - 1 && maze[i][j+1] != '0') {
-            if (dfs(visited, path, nodes, i, j + 1, endX, endY)) {
-                return true;
-            }
-        }
-        if (i < maze.size() && maze[i+1][j] != '0') {
-            if (dfs(visited, path, nodes, i + 1, j, endX, endY)) {
-                return true;
-            }
-        }
-        path.pop_back();
-        visited[i][j] = false;
     }
     return false;
 }
 
-void applyDFS(int i, int j, int endX, int endY) {
-    vector<vector<bool>> visited(maze.size(), vector<bool>(maze[0].size(), false));
-
-    vector<pair<int, int>> path;
+void applyBFS(int i, int j, int iEnd, int jEnd) {
+    vector<vector<int> > path;
+    map<vector<int>, vector<vector<int>> > m;
+    int cost = 0;
     int nodes = 0;
 
-    if (dfs(visited, path, nodes, i, j, endX, endY)) {
+    if (bfs(path, m, cost, nodes, i, j, iEnd, jEnd)) {
+        cout << "Path: ";
+        for (auto it = m.begin(); it != m.end(); ++it) {
+            cout << "(" << it->first[0] << "," << it->first[1] << ") : ";
+            for (int i = 0; i < it->second.size(); ++i) {
+                cout << "(" << it->second[i][0] << "," << it->second[i][1] << ") ";
+            }
+            cout << endl;
+        }
+        cout << "Cost: \t" << cost << endl;
+        cout << "Nodes: \t" << nodes << endl << endl;
+    }
+}
+
+bool dfs(vector<vector<bool> > visited, vector<pair<int, int> > &path, int &cost, int &nodes, int i, int j, int iEnd, int jEnd) {
+    if (!visited[i][j]) {
+        visited[i][j] = true;
+        path.push_back(make_pair(i, j));
+        if (maze[i][j] == '*') {
+            cost += 30;
+        } else {
+            ++cost;
+        }
+        ++nodes;
+        if (i == iEnd && j == jEnd) {
+            return true;
+        }
+        if (j > 0 && maze[i][j-1] != '0') {
+            if (dfs(visited, path, cost, nodes, i, j - 1, iEnd, jEnd)) {
+                return true;
+            }
+        }
+        if (i > 0 && maze[i-1][j] != '0') {
+            if (dfs(visited, path, cost, nodes, i - 1, j, iEnd, jEnd)) {
+                return true;
+            }
+        }
+        if (j < maze[0].size() - 1 && maze[i][j+1] != '0') {
+            if (dfs(visited, path, cost, nodes, i, j + 1, iEnd, jEnd)) {
+                return true;
+            }
+        }
+        if (i < maze.size() && maze[i+1][j] != '0') {
+            if (dfs(visited, path, cost, nodes, i + 1, j, iEnd, jEnd)) {
+                return true;
+            }
+        }
+        visited[i][j] = false;
+        path.pop_back();
+        if (maze[i][j] == '*') {
+            cost -= 30;
+        } else {
+            --cost;
+        }
+        --nodes;
+    }
+    return false;
+}
+
+void applyDFS(int i, int j, int iEnd, int jEnd) {
+    vector<vector<bool> > visited(maze.size(), vector<bool>(maze[0].size(), false));
+
+    vector<pair<int, int> > path;
+    int cost = 0;
+    int nodes = 0;
+
+    if (dfs(visited, path, cost, nodes, i, j, iEnd, jEnd)) {
         cout << "Path: ";
         for (int i = 0; i < path.size(); ++i) {
             cout << '\t' << "(" << path[i].first << "," << path[i].second << ")" << endl;
         }
-        cout << "Cost: \t" << path.size() << endl;
+        cout << "Cost: \t" << cost << endl;
         cout << "Nodes: \t" << nodes << endl << endl;
     }
 
@@ -91,11 +161,17 @@ void applyDFS(int i, int j, int endX, int endY) {
 }
 
 int main() {
-    cout << "Depth-First Search" << endl << endl;
+    cout << "Breadth First Search" << endl << endl;
 
-    applyDFS(13, 2, 5, 23);
-    applyDFS(13, 2, 3, 2);
-    applyDFS(24, 0, 0, 24);
+    applyBFS(13, 2, 5, 23);
+    // applyBFS(13, 2, 3, 2);
+    // applyBFS(24, 0, 0, 24);
+
+    // cout << "Depth First Search" << endl << endl;
+
+    // applyDFS(13, 2, 5, 23);
+    // applyDFS(13, 2, 3, 2);
+    // applyDFS(24, 0, 0, 24);
 
     return 0;
 }
