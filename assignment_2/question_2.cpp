761c2ff5157f9c71b0c1d65f67aab08092da7e7a
Paolo Torres <paolo-torres@hotmail.com>
2020-10-18 23:49:59 -0400

    finish a star and question 2

--- assignment_2/question_2.cpp
+++ assignment_2/question_2.cpp
@@ -35,18 +35,23 @@ const vector<vector<const char> > maze {
     {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}
 };
 
-vector<vector<int> > tracePath(map<vector<int>, vector<vector<int>> > &m, int &cost, int i, int j, int iEnd, int jEnd) {
+vector<vector<int> > tracePath(map<vector<int>, vector<vector<int>> > &m, 
+    int &cost, int iStart, int jStart, int iEnd, int jEnd) {
     vector<vector<int> > path; 
     int iTrace = iEnd;
     int jTrace = jEnd;
+
     path.push_back({iEnd, jEnd});
-    while (iTrace != i || jTrace != j) {
+    
+    while (iTrace != iStart || jTrace != jStart) {
         for (auto it = m.begin(); it != m.end(); ++it) {
             for (int v = 0; v < it->second.size(); ++v) {
                 if (iTrace == it->second[v][0] && jTrace == it->second[v][1]) {
                     iTrace = it->first[0];
                     jTrace = it->first[1];
+
                     path.push_back({iTrace, jTrace});
+
                     if (maze[iTrace][jTrace] == '*') {
                         cost += 30;
                     } else {
@@ -56,37 +61,57 @@ vector<vector<int> > tracePath(map<vector<int>, vector<vector<int>> > &m, int &c
             }
         }
     }
+
     reverse(path.begin(), path.end());
     return path;
 }
 
 bool isValid(int x, int y, vector<vector<bool> > &visited) {
-    return x >= 0 && x < maze.size() && y >= 0 && y < maze[0].size() && !visited[x][y] && maze[x][y] != '0';
+    return x >= 0 && x < maze.size() && y >= 0 && y < maze[0].size() 
+        && !visited[x][y] && maze[x][y] != '0';
 }
 
-bool bfs(vector<vector<int> > &path, map<vector<int>, vector<vector<int>> > &m, int &cost, int &nodes, int i, int j, int iEnd, int jEnd) {
+void displayData(vector<vector<int> >& path, int &cost, int &nodes) {
+    cout << "Path: ";
+    for (int i = 0; i < path.size(); ++i) {
+        cout <<  "(" << path[i][0] << ", " << path[i][1] << "), ";
+    }
+    cout << endl;
+    cout << "Cost: " << cost << endl;
+    cout << "Nodes: " << nodes << endl << endl;
+}
+
+bool bfs(vector<vector<int> > &path, map<vector<int>, vector<vector<int>> > &m, 
+    int &cost, int &nodes, int i, int j, int iEnd, int jEnd) {
     vector<vector<bool> > visited(maze.size(), vector<bool>(maze[0].size(), false));
     vector<vector<int> > dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
     queue<vector<int> > q;
+
     visited[i][j] = true;
     q.push({i, j});
+
     while (!q.empty()) {
         vector<int> s = q.front();
         q.pop();
+
         if (s[0] == iEnd && s[1] == jEnd) {
             path = tracePath(m, cost, i, j, iEnd, jEnd);
             return true;
         }
+
         for (int i = 0; i < dirs.size(); ++i) {
             int x = s[0] + dirs[i][0];
             int y = s[1] + dirs[i][1];
+
             if (isValid(x, y, visited)) {
                 visited[x][y] = true;
                 q.push({x, y});
+
                 if (m.find({s[0], s[1]}) == m.end()) {
                     m.insert({{s[0], s[1]}, vector<vector<int>>()});
                 }
                 m[{s[0], s[1]}].push_back(q.back());
+
                 ++nodes;
             }
         }
@@ -101,29 +126,28 @@ void applyBFS(int i, int j, int iEnd, int jEnd) {
     int nodes = 0;
 
     if (bfs(path, m, cost, nodes, i, j, iEnd, jEnd)) {
-        cout << "Path: ";
-        for (int i = 0; i < path.size(); ++i) {
-            cout <<  "(" << path[i][0] << ", " << path[i][1] << ") > ";
-        }
-        cout << endl;
-        cout << "Cost: " << cost << endl;
-        cout << "Nodes: " << nodes << endl << endl;
+        displayData(path, cost, nodes);
     }
 }
 
-bool dfs(vector<vector<bool> > visited, vector<pair<int, int> > &path, int &cost, int &nodes, int i, int j, int iEnd, int jEnd) {
+bool dfs(vector<vector<bool> > visited, vector<vector<int> > &path, 
+    int &cost, int &nodes, int i, int j, int iEnd, int jEnd) {
     if (!visited[i][j]) {
         visited[i][j] = true;
-        path.push_back(make_pair(i, j));
+        path.push_back({i, j});
+
         if (maze[i][j] == '*') {
             cost += 30;
         } else {
             ++cost;
         }
+
         ++nodes;
+
         if (i == iEnd && j == jEnd) {
             return true;
         }
+
         if (j > 0 && maze[i][j-1] != '0') {
             if (dfs(visited, path, cost, nodes, i, j - 1, iEnd, jEnd)) {
                 return true;
@@ -144,13 +168,16 @@ bool dfs(vector<vector<bool> > visited, vector<pair<int, int> > &path, int &cost
                 return true;
             }
         }
+
         visited[i][j] = false;
         path.pop_back();
+
         if (maze[i][j] == '*') {
             cost -= 30;
         } else {
             --cost;
         }
+
         --nodes;
     }
     return false;
@@ -158,19 +185,12 @@ bool dfs(vector<vector<bool> > visited, vector<pair<int, int> > &path, int &cost
 
 void applyDFS(int i, int j, int iEnd, int jEnd) {
     vector<vector<bool> > visited(maze.size(), vector<bool>(maze[0].size(), false));
-
-    vector<pair<int, int> > path;
+    vector<vector<int> > path;
     int cost = 0;
     int nodes = 0;
 
     if (dfs(visited, path, cost, nodes, i, j, iEnd, jEnd)) {
-        cout << "Path: ";
-        for (int i = 0; i < path.size(); ++i) {
-            cout << "(" << path[i].first << ", " << path[i].second << ") > ";
-        }
-        cout << endl;
-        cout << "Cost: " << cost << endl;
-        cout << "Nodes: " << nodes << endl << endl;
+        displayData(path, cost, nodes);
     }
 
     for (auto &v: visited) {
@@ -179,6 +199,74 @@ void applyDFS(int i, int j, int iEnd, int jEnd) {
 
     path.clear();
     nodes = 0;
+}
+
+int getGCost(int iStart, int jStart, int i, int j) {
+    return abs(i - iStart) + abs(j - jStart);
+}
+
+int getHCost(int i, int j, int iEnd, int jEnd) {
+    return abs(i - iEnd) + abs(j - jEnd);
+}
+
+int getFCost(int iStart, int jStart, int i, int j, int iEnd, int jEnd) {
+    return getGCost(iStart, jStart, i, j) + getHCost(i, j, iEnd, jEnd);
+}
+
+bool aStar(vector<vector<int> > &path, map<vector<int>, vector<vector<int>> > &m, 
+    int &cost, int &nodes, int iStart, int jStart, int iEnd, int jEnd) {
+    vector<vector<bool> > visited(maze.size(), vector<bool>(maze[0].size(), false));
+    vector<vector<int> > dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, 
+        greater<pair<int, pair<int, int>>> > pq;
+
+    visited[iStart][jStart] = true;
+    int fCost = getFCost(iStart, jStart, iStart, jStart, iEnd, jEnd);
+    pq.push(make_pair(fCost, make_pair(iStart, jStart)));
+    
+    while (!pq.empty()) {
+        pair<int, int> s = pq.top().second;
+        pq.pop();
+
+        if (s.first == iEnd && s.second == jEnd) {
+            path = tracePath(m, cost, iStart, jStart, iEnd, jEnd);
+            return true;
+        }
+
+        for (int i = 0; i < dirs.size(); ++i) {
+            int x = s.first + dirs[i][0];
+            int y = s.second + dirs[i][1];
+
+            if (isValid(x, y, visited)) {
+                visited[x][y] = true;
+                int fCost = getFCost(iStart, jStart, x, y, iEnd, jEnd);
+
+                if (maze[x][y] == '*') {
+                    fCost += 30;
+                }
+                pq.push(make_pair(fCost, make_pair(x, y)));
+
+                if (m.find({s.first, s.second}) == m.end()) {
+                    m.insert({{s.first, s.second}, vector<vector<int>>()});
+                }
+                m[{s.first, s.second}].push_back({x, y});
+
+                ++nodes;
+            }
+        }
+    }
+    return false;
+}
+
+void applyAStar(int i, int j, int iEnd, int jEnd) {
+    vector<vector<int> > path;
+    map<vector<int>, vector<vector<int>> > m;
+    int cost = 0;
+    int nodes = 0;
+
+    if (aStar(path, m, cost, nodes, i, j, iEnd, jEnd)) {
+        displayData(path, cost, nodes);
+    }
 }
 
 int main() {
@@ -193,6 +281,12 @@ int main() {
     applyDFS(13, 2, 5, 23);
     applyDFS(13, 2, 3, 2);
     applyDFS(24, 0, 0, 24);
+
+    cout << "A* Search" << endl << endl;
+
+    applyAStar(13, 2, 5, 23);
+    applyAStar(13, 2, 3, 2);
+    applyAStar(24, 0, 0, 24);
 
     return 0;
 }
