b1f43d84052b95eeff29b2f2e7786f25354f4752
Paolo Torres <paolo-torres@hotmail.com>
2020-11-25 17:59:32 -0500

    init genetic programming fitness, evaluate, actual output

---
+++ assignment_4/question_3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+constexpr auto CASES = 64;
+
+const int fitness(vector<bool>& values) {
+    auto fitness = 0;
+    for (int i = 0; i < CASES; i++) {
+        auto currentOutput = evaluate(values, i);
+        if (currentOutput == actualOutput(values)) {
+            fitness++;
+        }
+    }
+    return fitness;
+}
+
+const bool evaluate(vector<bool>& values, int i) {
+    /*
+				a0  a1  d0  d1  d2  d3
+		i = 0:  0   0   0   0   0   0
+		i = 1:  0   0   0   0   0   1
+		i = 2:  0   0   0   0   1   0
+		i = 3:  0   0   0   0   1   1
+		i = 4:  0   0   0   1   0   0
+		i = 5:  0   0   0   1   0   1
+		i = 6:  0   0   0   1   1   0
+		i = 7:  0   0   0   1   1   1
+		i = 8:  0   0   1   0   0   0
+		i = 9:  0   0   1   0   0   1
+		...
+	*/
+
+    return true;
+}
+
+const bool actualOutput(vector<bool>& values) {
+	/*
+		a0  a1  d0  d1  d2  d3
+		0   0   1   0   0   0
+		0   1   0   1   0   0
+		1   0   0   0   1   0
+		1   1   0   0   0   1
+	*/
+
+    if (values[0] && values[1]) {
+        return values[5];
+    }
+    if (values[0]) {
+        return values[4];
+    }
+    if (values[1]) {
+        return values[3];
+    }
+    return values[2];
+}
+
+int main() {
+    cout << "Genetic Programming" << endl;
+
+    return 0;
+}
