abc161b17801112975325279b954d2ef37df56c9
Paolo Torres <paolo-torres@hotmail.com>
2020-11-29 21:19:18 -0500

    finish crossover and implement mutation based on probabilities

--- assignment_4/question_3.cpp
+++ assignment_4/question_3.cpp
@@ -107,7 +107,8 @@ const void generate(TreeNode* root, mt19937& rng, const int maxDepth) {
 	}
 }
 
-const void storeFunctions(TreeNode* head, vector<vector<string>>& initFunctions, int i) {
+const void storeFunctions(TreeNode* head, vector<vector<string>>& initFunctions,
+	int i) {
 	if (head == NULL) {
 		return;
 	}
@@ -116,7 +117,8 @@ const void storeFunctions(TreeNode* head, vector<vector<string>>& initFunctions,
 	storeFunctions(head->right, initFunctions, i);
 }
 
-const void storeTerminals(vector<string>& initTerminals, mt19937& rng, const int maxDepth) {
+const void storeTerminals(vector<string>& initTerminals, mt19937& rng,
+	const int maxDepth) {
 	initTerminals = terminals;
 	uniform_int_distribution<int> uniInputs(0, 1);
 	int n = (maxDepth + 1) / 2;
@@ -161,26 +163,70 @@ const void displayTrees(vector<vector<string>>& solutions) {
 	cout << endl;
 }
 
-const int evaluate(vector<string>& solution, vector<string>& correctProgram) {
-    auto fitness = 0;
+const double evaluate(vector<string>& solution, vector<string>& correctProgram) {
+    double fitness = 0;
     for (int i = 0; i < solution.size(); ++i) {
         if (solution[i] == correctProgram[i]) {
             ++fitness;
         }
     }
-    return fitness;
+    return fitness / solution.size();
+}
+
+const void performCrossover(vector<vector<string>>& solutions, int i,
+	mt19937& rng, vector<string>& correctProgram) {
+	uniform_int_distribution<int> uniCrossover(i + 1, solutions.size() - 1);
+	auto crossoverIndex = uniCrossover(rng);
+
+	auto firstFitness = evaluate(solutions[i], correctProgram);
+	auto secondFitness = evaluate(solutions[crossoverIndex], correctProgram);
+
+	auto firstElement = rand() * solutions[i].size() / RAND_MAX;
+	auto secondElement = rand() * (solutions[crossoverIndex].size() - 1) / RAND_MAX;
+	if (secondElement >= firstElement) {
+		++secondElement;
+	}
+
+	swap(solutions[i][firstElement], solutions[crossoverIndex][secondElement]);
+	auto newFirstFitness = evaluate(solutions[i], correctProgram);
+	auto newSecondFitness = evaluate(solutions[crossoverIndex], correctProgram);
+
+	if (firstFitness + secondFitness > newFirstFitness + newSecondFitness) {
+		swap(solutions[i][firstElement], solutions[crossoverIndex][secondElement]);
+	}
+}
+
+const void performMutation(vector<vector<string>>& solutions, int i,
+	mt19937& rng, vector<string>& correctProgram) {
+	auto fitness = evaluate(solutions[i], correctProgram);
+
+	auto element = rand() * solutions[i].size() / RAND_MAX;
+	if (find(functions.begin(), functions.end(),
+		solutions[i][element]) != functions.end()) {
+		uniform_int_distribution<int> uniFunctions(0, functions.size() - 1);
+		auto functionsIndex = uniFunctions(rng);
+
+		solutions[i][element] = functions[functionsIndex];
+	} else {
+		uniform_int_distribution<int> uniTerminals(0, terminals.size() - 1);
+		auto terminalsIndex = uniTerminals(rng);
+
+		solutions[i][element] = terminals[terminalsIndex];
+	}
+	auto temp = solutions[i][element];
+	auto newFitness = evaluate(solutions[i], correctProgram);
+	if (fitness > newFitness) {
+		solutions[i][element] = temp;
+	}
 }
 
 const void evolve(vector<vector<string>>& solutions, const int numGenerations,
 	const double crossoverProbability, const double mutationProbability, 
-	mt19937& rng, vector<string>& correctProgram, int& correctFitness, 
-	vector<string>& bestProgram, int& bestFitness) {
+	mt19937& rng, vector<string>& correctProgram, double& correctFitness, 
+	vector<string>& bestProgram, double& bestFitness) {
 
-	int generation = 0;
-	while (generation < numGenerations) {
-		uniform_int_distribution<int> uniBinary(0, 1);
-		auto variationOperator = uniBinary(rng);
-
+	int generation = 1;
+	while (generation <= numGenerations) {
 		uniform_real_distribution<> uniOperator(0, 1);
 		auto probability = uniOperator(rng);
 
@@ -192,51 +238,40 @@ const void evolve(vector<vector<string>>& solutions, const int numGenerations,
 			}
 		}
 
-		if (variationOperator == 0) {
-			if (probability < crossoverProbability) {
-				auto first = rand() * solutions.size() / RAND_MAX;
-				auto second = rand() * (solutions.size() - 1) / RAND_MAX;
-				if (second >= first) {
-					++second;
+		for (int i = 0; i < solutions.size() - 1; ++i) {
+			uniform_int_distribution<int> uniBinary(0, 1);
+			auto variationOperator = uniBinary(rng);
+
+			if (variationOperator == 0) {
+				if (probability < crossoverProbability) {
+					performCrossover(solutions, i, rng, correctProgram);
 				}
-				
-				auto firstParent = solutions[first];
-				auto secondParent = solutions[second];
-
-				auto firstFitness = evaluate(firstParent, correctProgram);
-				auto secondFitness = evaluate(secondParent, correctProgram);
-
-				auto firstElement = rand() * firstParent.size() / RAND_MAX;
-				auto secondElement = rand() * secondParent.size() / RAND_MAX;
-				if (secondElement >= firstElement) {
-					++secondElement;
+			} else {
+				if (probability < mutationProbability) {
+					performMutation(solutions, i, rng, correctProgram);
 				}
-
-				swap(firstParent[firstElement], secondParent[secondElement]);
-				auto newFirstFitness = evaluate(firstParent, correctProgram);
-				auto newSecondFitness = evaluate(secondParent, correctProgram);
-
-				if (firstFitness + secondFitness > newFirstFitness + newSecondFitness) {
-					swap(firstParent[firstElement], secondParent[secondElement]);
-				}
-			}
-		} else {
-			if (probability < mutationProbability) {
-				// Apply Mutation
 			}
 		}
+
+		cout << generation << ": ";
+		for (int i = 0; i < bestProgram.size(); ++i) {
+			cout << bestProgram[i] << " ";
+		}
+		cout << "Fitness: " << bestFitness << endl;
+
 		++generation;
 	}
+	cout << endl;
 }
 
 int main() {
 	random_device rd;
 	mt19937 rng(rd());
 
-	const auto populationSize = 10;
-	const auto numGenerations = 500;
+	const auto populationSize = 100;
+	const auto numGenerations = 1000;
 	const auto crossoverProbability = 0.9;
-	const auto mutationProbability = 0.05;
+	const auto mutationProbability = 0.6;
 	const auto maxDepth = 7;
 
 	vector<vector<string>> initFunctions(populationSize, vector<string>());
@@ -245,31 +280,30 @@ int main() {
 
 	generateTrees(initFunctions, initTerminals, populationSize, maxDepth, rng);
 	combineTrees(initFunctions, initTerminals, solutions, populationSize);
-	displayTrees(solutions);
 
 	vector<string> correctProgram = {"IF", "AND", "IF", "AND", "NOT", "IF",
 		"AND", "NOT", "a0", "a1", "d3", "a0", "a1", "d2", "a0", "a1", "d1", "d0"};
-	auto correctFitness = 64;
+	double correctFitness = 1;
 
 	vector<string> bestProgram = {};
-	auto bestFitness = 0;
+	double bestFitness = 0;
 
-	evolve(solutions, numGenerations, crossoverProbability, 
-		mutationProbability, rng, correctProgram, correctFitness, bestProgram, 
-		bestFitness);
+	evolve(solutions, numGenerations, crossoverProbability, mutationProbability,
+		rng, correctProgram, correctFitness, bestProgram, bestFitness);
 
-	cout << "Correct:\t";
+	cout << "Correct Program: ";
 	for (int i = 0; i < correctProgram.size(); ++i) {
 		cout << correctProgram[i] << " ";
 	}
-	cout << endl << endl;
+	cout << endl;
+	cout << "Correct Fitness: " << correctFitness << endl << endl;
 
-	cout << "Best Program:\t";
+	cout << "Best Program: ";
 	for (int i = 0; i < bestProgram.size(); ++i) {
 		cout << bestProgram[i] << " ";
 	}
 	cout << endl;
-	cout << "Best Fitness:\t" << bestFitness << endl;
+	cout << "Best Fitness: " << bestFitness << endl;
 
     return 0;
 }
